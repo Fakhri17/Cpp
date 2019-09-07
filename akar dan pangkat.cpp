af73f9b18a9a41aca7a9adfccb47351b45795e3a
Fakhri Alauddin <54692387+Fakhri17@users.noreply.github.com>
2019-09-07 22:11:48 +0700

    Add files via upload

---
+++ akar dan pangkat.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+#include <conio.h>
+using namespace std;
+
+int main(){
+	
+	float a,b,c;
+		cout << " masukkan bilangan "; cin>>a;
+		c=pow(a,(float)1/2);
+		cout<<" hasil akar "<<c<<endl<<endl;
+			
+		cout << " masukkan bilangan "; cin>>a;
+		c=pow(a,(float)1/3);
+		cout<<" hasil akar 3 = "<<c<<endl<<endl;
+		
+		cout << " masukkan bilangan "; cin>>a;
+		c=pow(a,(float)1/4);
+		cout<<" hasil akar 4 = "<<c<<endl<<endl;
+	
+	
+	getch ();
+}
+
